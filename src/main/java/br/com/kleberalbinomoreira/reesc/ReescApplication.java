c914fb5621813a5dd25670b741c90e7fc387355e
kleber0a0m <kleber0a0m@gmail.com>
2023-03-12 16:52:01 -0300

    inicio

---
+++ src/main/java/br/com/kleberalbinomoreira/reesc/ReescApplication.java
@@ -0,0 +1,13 @@
+package br.com.kleberalbinomoreira.reesc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReescApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReescApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kleberalbinomoreira/reesc/ReescApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kleberalbinomoreira.reesc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReescApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
